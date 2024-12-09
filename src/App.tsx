b9760ade43163edbf80b08992264611616f16867
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-12-09 20:32:32 +0700

    App wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useMemo, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Cookies from "js-cookie";
+import theme from "./theme";
+import Home from "./components/pages/Home";
+import ShoeDetail from "./components/Shoe/ShoeDetail";
+import Nav from "./components/Header/Nav";
+import Footer from "./components/Footer/Footer";
+import FavoritePage from "./components/pages/FavoritePage";
+import CartPage from "./components/pages/CartPage";
+import CheckoutPage from "./components/pages/CheckoutPage";
+import {
+  AppWrapper,
+  ContentWrapper,
+} from "./components/styles/BasicComponents";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [mode, setMode] = useState<"light" | "dark">(
+    (Cookies.get("theme") as "light" | "dark") || "light"
+  );
+
+  // Memorize the theme to avoid re-render
+  const currentTheme = useMemo(() => theme(mode), [mode]);
+
+  const toggleTheme = () => {
+    const newMode = mode === "light" ? "dark" : "light";
+    setMode(newMode);
+    Cookies.set("theme", newMode, { expires: 365 });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <ThemeProvider theme={currentTheme}>
+        <CssBaseline />
+        <Nav onToggleTheme={toggleTheme} mode={mode} />
+        <AppWrapper>
+          <ContentWrapper>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/shoes/:id" element={<ShoeDetail />} />
+              <Route path="/favorite" element={<FavoritePage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+            </Routes>
+          </ContentWrapper>
+          <Footer />
+        </AppWrapper>
+      </ThemeProvider>
+    </Router>
+  );
+};
 
-export default App
+export default App;
